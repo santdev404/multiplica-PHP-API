62717e7f35da5c15c092f6292763401d1d69a30d
Santdev404 <santdev404@gmail.com>
2021-06-03 16:49:28 -0500

    Adding User, Model migrations and models

--- database/migrations/2021_06_03_213818_create_colors_table.php
+++ database/migrations/2021_06_03_213818_create_colors_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateColorsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('colors', function (Blueprint $table) {
             $table->id();
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->string('role');
-            $table->rememberToken();
+            $table->string('name');
+            $table->string('color');
+            $table->string('pantone');
+            $table->integer('year');
             $table->timestamps();
         });
     }
@@ -30,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('colors');
     }
 }
