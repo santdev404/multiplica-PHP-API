1404a6786c90b3e22033daf154b14dc748d934a5
Santdev404 <santdev404@gmail.com>
2021-06-03 18:16:56 -0500

    Config action register of UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,11 +7,75 @@ use App\Models\User;
 
 class UserController extends Controller
 {
-    public function login(){
+
+    public function register(Request $request){
+
+        $json = $request->input('json', null);
+        $params = json_decode($json);
+        $params_array = json_decode($json, true);
+
+        $params_array = array_map('trim',$params_array);
+
+        $validate = \Validator::make($params_array, [
+            'email'    => 'required|email|unique:users', 
+            'password' => 'required'
+        ]);
+
+
+        if(!empty($params) && !empty($params_array)){
+
+            if($validate->fails()){
+                $data = array(
+                    'status' => 'error',
+                    'code' => 404,
+                    'message' => 'El usuario no se ha creado',
+                    'errors' => $validate->errors()
+                );
+            }else{
+
+                $pwd = hash('sha256', $params->password);
+
+
+                $user = new User();
+                $user->email = $params_array['email'];
+                $user->password = $pwd;
+                $user->role = 'ROLE_USER';
+
+                $user->save();
+
+
+                $data = array(
+                    'status' => 'success',
+                    'code'   => 200,
+                    'message' => 'El usuario  se ha creado',
+                    'user' => $user
+                );
+            }
+
+        }else{
+            $data = array(
+                'status' => 'error',
+                'code' => 404,
+                'message' => 'Datos enviados no son correctos'
+            );
+        }
+        
+
+
+        return response()->json($data, $data['code']);
 
     }
 
-    public function register(){
+    public function login(Request $request){
 
+        $data = array(
+            'status' => 'success',
+            'code' => 200,
+            'message' => 'Ok'
+        );
+
+        return response()->json($data, $data['code']);
     }
+
+
 }
