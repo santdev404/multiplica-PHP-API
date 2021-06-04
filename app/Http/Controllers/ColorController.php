342de5f66e65e2b0e88cda5ace2f4d9e7de1e3ac
Santdev404 <santdev404@gmail.com>
2021-06-03 19:45:00 -0500

    Index action in ColorController

--- app/Http/Controllers/ColorController.php
+++ app/Http/Controllers/ColorController.php
@@ -6,10 +6,22 @@ use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 
 use App\Models\Color;
+use App\Helpers\JwtAuth;
 
 class ColorController extends Controller
 {
+    public function __construct(){
+        
+        $this->middleware('api.auth');
+    }
+
     public function index(){
+        $colors = Color::all();
+        return response()->json([
+            'code' => 200,
+            'status' => 'success',
+            'posts' => $colors
+        ], 200);
 
     }
 
