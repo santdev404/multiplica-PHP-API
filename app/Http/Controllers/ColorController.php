402163b8aac663e2e5e316a3335c87e9a0d7fbdf
Santdev404 <santdev404@gmail.com>
2021-06-03 20:07:09 -0500

    Color store action in ColorController

--- app/Http/Controllers/ColorController.php
+++ app/Http/Controllers/ColorController.php
@@ -46,8 +46,59 @@ class ColorController extends Controller
 
     }
 
-    public function store(){
+    public function store(Request $request){
+        
+        $json = $request->input('json', null);
+        $params = json_decode($json);
+        $params_array = json_decode($json, true);
 
+        if(!empty($params_array)){
+            
+            $validate = \Validator::make($params_array, [
+                'name' => 'required',
+                'color' => 'required',
+                'pantone' => 'required',
+                'year' => 'required'
+
+            ]);
+
+            if($validate->fails()){
+                $data = [
+                    'code'=> 400,
+                    'status' => 'error',
+                    'message' => 'Faltan datos'
+                ];
+            }else{
+                $color = new Color();
+
+                $color->name = $params->name;
+                $color->color = $params->color;
+                $color->pantone = $params->pantone;
+                $color->year = $params->year;
+
+                $color->save();
+
+                
+                $data = [
+                    'code'=> 200,
+                    'status' => 'success',
+                    'post' => $color
+                ];
+            }
+
+            
+        }else{
+            $data = [
+                'code'=> 400,
+                'status' => 'error',
+                'message' => 'Envia datos correctamente'
+            ];
+        }
+
+
+
+        //Devolver la respuesta
+        return response()->json($data, $data['code']);
     }
 
     public function update(){
